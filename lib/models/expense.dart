307c80b13c348fd3af98b173b53468c88e83e591
Ady <ady@dnsspecialistservices.com.au>
2023-09-28 08:07:38 +1000

    Introducing "enum" usage

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,10 +2,15 @@
 //Generating new unique id
 import 'package:uuid/uuid.dart';
 
-final uuid = Uuid();
+const uuid = Uuid();
 
-//In Dart, "initializer Lists" can be used to initialize class properties like id
-//with values that are NOT received as constructor function arguments
+///In Dart, "initializer Lists" can be used to initialize class properties like id
+///with values that are NOT received as constructor function arguments
+
+//enum allows to create custom TYPE and define values with curly braces.
+//Not wrapped as string but treated like string
+enum Category { food, travel , leisure, work}
+
 class Expense {
   //Not adding id to param coz wanna build unique id dynamically whenever new Expense is created
 
@@ -13,10 +18,12 @@ class Expense {
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();   //v4 method generate unique String uuid
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
